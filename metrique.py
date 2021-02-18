40b4a6e6189bc7a0adb90733c24f3e7c6ee5826a
daothauvin <49521593+daothauvin@users.noreply.github.com>
2021-02-18 15:10:14 +0100

    start metrique

---
+++ metrique.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 14:56:40 2021
+
+@author: dao
+"""
+
+class EvalMesure:
+    def evalQuery(liste,query):
+        pass
+
+class Précision:
+    def evalQuery(liste,query):
+        liste == query.pertinents
+    
+class Rappel:
+    def evalQuery(liste,query):
+        pass
+    
+class F_mesure:
+    def evalQuery(liste,query):
+        pass
