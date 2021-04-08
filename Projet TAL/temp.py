b0b808f371681d5d67c66a7b634aeaa45f21767d
Luc <luc.sterker@laposte.net>
2021-04-08 14:17:14 +0200

    projet open

---
+++ Projet TAL/temp.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import codecs
+import re
+import os.path
+
+# Chargement des données:
+def load_pres(fname):
+    alltxts = []
+    alllabs = []
+    s=codecs.open(fname, 'r','utf-8') # pour régler le codage
+    while True:
+        txt = s.readline()
+        if(len(txt))<5:
+            break
+        #
+        lab = re.sub(r"<[0-9]*:[0-9]*:(.)>.*","\\1",txt)
+        txt = re.sub(r"<[0-9]*:[0-9]*:.>(.*)","\\1",txt)
+        if lab.count('M') >0:
+            alllabs.append(-1)
+        else: 
+            alllabs.append(1)
+        alltxts.append(txt)
+    return alltxts,alllabs
+
+
+fname = "AFDpresidentutf8/corpus.tache1.learn.utf8"
+
+alltxts,alllabs = load_pres(fname)
+
+fname = "AFDpresidentutf8/corpus.tache1.test.utf8"
+
+alltxts_test,alllabs_test = load_pres(fname)
+
+print(len(alltxts),len(alllabs))
+print(alltxts[0])
+print(alllabs[0])
+print(alltxts[-1])
+print(alllabs[-1])
+
+def load_movies(path2data): # 1 classe par répertoire
+    alltxts = [] # init vide
+    labs = []
+    cpt = 0
+    for cl in os.listdir(path2data): # parcours des fichiers d'un répertoire
+        for f in os.listdir(path2data+cl):
+            txt = open(path2data+cl+'/'+f).read()
+            alltxts.append(txt)
+            labs.append(cpt)
+        cpt+=1 # chg répertoire = cht classe
+        
+    return alltxts,labs
+
+
+path = "AFDmovies/movies1000/"
+
+alltxts,alllabs = load_movies(path)
